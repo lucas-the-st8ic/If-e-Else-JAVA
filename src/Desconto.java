061c906199de1f6983232e48a814dee6bc62e89e
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-11-06 17:22:32 -0300

    Update Desconto.java

--- src/Desconto.java
+++ src/Desconto.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Desconto {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+
+        double desconto = 10;
+        double valor;
+        double valorDoDesconto;
+        double valorFinal;
+
+        System.out.println("Digite o valor da compra: R$");
+        valor = input.nextDouble();
+
+        if (valor >= 100) {
+            valorDoDesconto =((double) desconto /100) * valor;
+            valorFinal =  valor - valorDoDesconto;
+            System.out.println(valorDoDesconto);
+
+            System.out.println("Desconto de " + desconto + "% aplicado.");
+            System.out.println("Valor da compra: R$" + valorFinal);
+        } else {
+            System.out.println("Nenhum desconto aplicado.");
+            System.out.println("Valor da compra: R$" + valor);
+        }
+    }
 }
