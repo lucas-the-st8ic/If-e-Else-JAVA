4561b0b3015b1b538b95fe544a3f6fdb6d70c083
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-11-07 15:45:07 -0300

    Update DoacaoDeSangue.java

--- src/DoacaoDeSangue.java
+++ src/DoacaoDeSangue.java
@@ -10,12 +10,14 @@ public class DoacaoDeSangue {
         System.out.println("Digite a idade do doador: ");
         idade = input.nextInt();
 
-        System.out.println("Digite o peso do doador: :");
+        System.out.println("Digite o peso do doador(em KG): ");
+        peso = input.nextInt();
 
         if (idade > idadeMinima && idade < idadeMaxima && peso > pesoMinimo) {
             System.out.println("Doador compatível com os requisitos.");
-        } else if (idade < idadeMinima && idade > idadeMaxima) {
-
+        } else if (idade < idadeMinima && idade > idadeMaxima && peso < pesoMinimo) {
+            System.out.println("Doador incompatível!!\nIdade mínima precisa estar entre" +idadeMinima+ " e " +idadeMaxima+ " anos.");
+            System.out.println("E o doador precisa pesar mais do que " +pesoMinimo+ " Kilos.");
         }
 
     }
