98f2d19f208485a3903f36716720fa23f7c6ce5f
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-11-07 13:34:10 -0300

    Create DoacaoDeSangue.java

---
+++ src/DoacaoDeSangue.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DoacaoDeSangue {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+
+        int idade, idadeMinima = 17, idadeMaxima = 65;
+        int peso, pesoMinimo = 50;
+
+        System.out.println("Digite a idade do doador: ");
+        idade = input.nextInt();
+
+        System.out.println("Digite o peso do doador: :");
+
+        if (idade > idadeMinima && idade < idadeMaxima && peso > pesoMinimo) {
+            
+        }
+
+    }
+}
